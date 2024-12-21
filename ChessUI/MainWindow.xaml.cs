a8d5a9f573d013c10bb4a9c2e9010ab5a6fc53cf
Joshua Byrd <jsbyrd02@gmail.com>
2024-12-21 18:49:25 -0500

    Add special moves en passant and king/queen side castling

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -216,4 +216,27 @@ public partial class MainWindow : Window
         DrawBoard(gameState.Chessboard);
         SetCursor(gameState.CurrentPlayer);
     }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (!IsMenuOnScreen() && e.Key == Key.Escape)
+        {
+            ShowPauseMenu();
+        }
+    }
+
+    private void ShowPauseMenu()
+    {
+        PauseMenu pauseMenu = new PauseMenu();
+        MenuContainer.Content = pauseMenu;
+        pauseMenu.OptionSelected += option =>
+        {
+            MenuContainer.Content = null;
+
+            if (option == Option.Restart)
+            {
+                RestartGame();
+            }
+        };
+    }
 }
